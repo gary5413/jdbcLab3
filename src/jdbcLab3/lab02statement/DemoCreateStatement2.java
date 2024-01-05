30ea156c57cbcc5f3db4c12b80390402099f7122
iiieduzl <iiieduzl@gmail.com>
2024-01-05 14:15:06 +0800

    更新lab02

--- src/jdbcLab3/lab02statement/DemoCreateStatement2.java
+++ src/jdbcLab3/lab02statement/DemoCreateStatement2.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
@@ -14,6 +15,7 @@ public class DemoCreateStatement2 {
 	FileInputStream fileInputStream = null;
 	Connection connection = null;
 	Statement statement = null;
+	ResultSet rs=null;
 
 	public void createConnection() {
 		try {
@@ -59,13 +61,16 @@ public class DemoCreateStatement2 {
 			if (statement != null) {
 				statement.close();
 			}
+			if(rs!=null) {
+				rs.close();
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-
+// 	新增
 	public void insert() {
 //		創建statement
 		String sql = "INSERT INTO users(name,password,email)VALUES('test','123456','test@mail.com')";
@@ -77,7 +82,87 @@ public class DemoCreateStatement2 {
 			e.printStackTrace();
 		}
 	}
-
+	
+//	刪除
+	public void delete() {
+		String sql="DELETE FROM　users WHERE id =6";
+		try {
+			statement= connection.createStatement();
+//			回傳int 代表已執行筆數
+			int row = statement.executeUpdate(sql);
+			System.out.println("刪除"+row+"筆");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+//	更新
+	/*
+	 * 讓同學動手實作
+	 * 第一筆資料 apple 改為 banana 
+	 */
+	
+	public void update() {
+		String sql="UPDATE users SET　name='banana' WHERE id=1";
+		try {
+			statement=connection.createStatement();
+			int row = statement.executeUpdate(sql);
+			System.out.println("更新"+row+"筆");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+//	查 搜尋
+	public void queryUsers() {
+//		關於* 有部分資深工程師會認為 如果你搜尋的資料不需要全部的話能不要用*就不要用 可能會影響效能
+		String sql="SELECT * FROM users";
+		try {
+			statement=connection.createStatement();
+//			回傳ResultSet 結果集
+			rs = statement.executeQuery(sql);
+//			next()方法 確認有無下一筆資料 回傳布林值
+//			boolean result = rs.next();
+//			System.out.println("result:"+result);
+			while(rs.next()) {
+				System.out.println(rs.getInt(1)+","+rs.getString(2)+","+rs.getString("password")+","+rs.getString("email"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+//		finally {
+//			try {
+//				rs.close();
+//			} catch (SQLException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//		}
+	}
+	
+	/*
+	 * 給同學操作
+	 * 搜尋id為1的單筆資料
+	 */
+	public void findUserById() {
+		String sql="SELECT name,password,email FROM users WHERE id =1";
+		try {
+			statement=connection.createStatement();
+			rs =statement.executeQuery(sql);
+			rs.next();
+//			順便示範搜尋不同資料 索引不同
+			System.out.println(rs.getString(1)+","+rs.getString("password"));
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 
 	public static void main(String[] args) {
 		/*
@@ -87,7 +172,11 @@ public class DemoCreateStatement2 {
 		 */
 		DemoCreateStatement2 demo2 = new DemoCreateStatement2();
 		demo2.createConnection();
-		demo2.insert();
+//		demo2.insert(); 
+//		demo2.delete();
+//		demo2.update();
+//		demo2.queryUsers();
+		demo2.findUserById();
 		demo2.closeConection();
 	
 	}
